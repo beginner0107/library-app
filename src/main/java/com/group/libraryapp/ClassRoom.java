fcad9d939891bae63afaac7c77a95ef989dfc584
Beginner <dkstmdwn17@naver.com>
2023-02-27 19:11:50 +0900

    study: 학습용으로 만들어 두었던 클래스 삭제

--- src/main/java/com/group/libraryapp/ClassRoom.java
+++
@@ -1,9 +0,0 @@
-package com.group.libraryapp;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class ClassRoom {
-    private String name;
-    private List<Student> students = new ArrayList<>();
-}
--- src/main/java/com/group/libraryapp/Student.java
+++
@@ -1,6 +0,0 @@
-package com.group.libraryapp;
-
-public class Student {
-    private String name;
-    private ClassRoom classRoom;
-}
