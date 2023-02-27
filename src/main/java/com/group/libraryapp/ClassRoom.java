f8d8ad7ec61bf06a87d16219d295b5caade8b409
Beginner <dkstmdwn17@naver.com>
2023-02-27 18:52:46 +0900

    study: 관계형 데이터베이스(양방향X)와, 객체와의 패러다임의 차이를 보여주는 코드

---
+++ src/main/java/com/group/libraryapp/ClassRoom.java
@@ -0,0 +1,9 @@
+package com.group.libraryapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassRoom {
+    private String name;
+    private List<Student> students = new ArrayList<>();
+}
