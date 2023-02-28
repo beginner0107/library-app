537443a4af885980370482a97e52eac2e44d3d1b
Beginner <dkstmdwn17@naver.com>
2023-03-01 01:23:54 +0900

    feature: 책 생성 API 구현
    `책` 엔티티를 만들고,
    서비스와 레포지토리를 작성
    CRUD의 Create를 구현

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,6 +1,8 @@
 package com.group.libraryapp.service.book;
 
-import com.group.libraryapp.repository.book.BookRepository;
+import com.group.libraryapp.domain.book.Book;
+import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.dto.book.request.BookCreateRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +14,7 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    public void saveBook() {
-        bookRepository.saveBook();
+    public void saveBook(BookCreateRequest request) {
+        bookRepository.save(new Book(request.getName()));
     }
 }
