d1b2418d4b51cf2c21b8fbde5e813dce02c5955a
Beginner <dkstmdwn17@naver.com>
2023-02-28 17:54:17 +0900

    feature: 서비스 계층에 `@Transactional` 추가
    `@Transaction`을 사용하여
    
    1. 더티 체킹(Dirty Checking)
    2. 쓰기 지연
    3. 1차 캐싱
    4. 지연로딩
    
    전략을 사용할 수 있다.

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,25 +21,30 @@ public class UserServiceV2 {
     }
 
     // save 메소드에 객체를 넣어주면 INSERT SQL이 자동으로 날라간다.
+    // 아래 있는 함수가 시작될 때 start transaction; 을 해준다. (트랜잭션을 시작!)
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         // select * from user where id = ?;
         // Optional<User>
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalAccessError::new);
         user.updateName(request.getName()); // 객체를 업데이트
-        userRepository.save(user); // save 메소드를 호출
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // select * from user where name = ?
         User user = userRepository.findByName(name)
